58fafcaff121cd9c8439943f0c0dd38bec12495e
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-13 23:39:10 +0530

    dataType

---
+++ dataType.java
@@ -0,0 +1,16 @@
+
+public class dataType {
+
+    public static void main(String[] args) {
+        // byte x = 2;
+        // System.out.println(x);
+        // System.out.println(y);
+        double myDouble = 3.8462;
+        int myInt = (int) myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+
+    }
+
+}
