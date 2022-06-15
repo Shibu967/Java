2315c8ecf1fb43844aeaf756703a531b4075e05b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 11:32:59 +0530

    ArithmaticIncrement

---
+++ ArithmaticIncrement.java
@@ -0,0 +1,9 @@
+public class ArithmaticIncrement {
+    public static void main(String[] args) {
+        // int a=10;
+        int b = 20;
+        // int c = b++;
+        int d = ++b;
+        System.out.println(d);
+    }
+}
