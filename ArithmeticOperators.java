fc4a04ab50c24f215f788f5bb2edacb26eebe534
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 11:33:41 +0530

    ArithmeticOperators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,8 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 800;
+        int b = 400;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
