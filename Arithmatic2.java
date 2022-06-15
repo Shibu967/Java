59aebad2e178733e8b0d0c2f2afdc8644d54042d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 11:32:30 +0530

    multiplication

---
+++ Arithmatic2.java
@@ -0,0 +1,9 @@
+public class Arithmatic2 {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 45;
+        int d = (a + b) * (a + b);
+        System.out.println(d);
+
+    }
+}
