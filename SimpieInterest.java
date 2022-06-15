3feee59fb2007274a07cf6b5cba06f06f15eda5e
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 12:35:23 +0530

    SimpieInterest

---
+++ SimpieInterest.java
@@ -0,0 +1,11 @@
+public class SimpieInterest {
+    public static void main(String[] args) {
+        int principal = 5000;
+        float rate = 12.5f;
+        int time = 12;
+
+        float SimpieInterest = principal * rate * time / 100;
+        System.out.println(SimpieInterest);
+
+    }
+}
