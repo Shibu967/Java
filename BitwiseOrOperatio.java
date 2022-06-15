8cd35338868724c3b2f5bce37a34d4b3c56288ef
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 12:32:30 +0530

    BitwiseOrOperatio

---
+++ BitwiseOrOperatio.java
@@ -0,0 +1,8 @@
+public class BitwiseOrOperatio {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        int c = a | b;
+        System.out.println(c);
+    }
+}
