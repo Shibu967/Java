31e53e2ef798232ac1660c1e29eb97e0cace1fde
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 12:32:57 +0530

    BitWiseRightSift

---
+++ BitWiseRightSift.java
@@ -0,0 +1,9 @@
+public class BitWiseRightSift {
+    public static void main(String[] args) {
+
+        // int a=10;
+        int b = 13;
+        int c = b >> 2;
+        System.out.println(c);
+    }
+}
