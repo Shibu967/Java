73b7f1b20d5fdbc2de410850d183972fe70ae909
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 11:34:09 +0530

    WholeSquare

---
+++ WholeSquare.java
@@ -0,0 +1,8 @@
+public class WholeSquare {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 9;
+        int c = a * a + b * b + 2 * a * b;
+        System.out.println(c);
+    }
+}
