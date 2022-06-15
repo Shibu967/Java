e0511697535a5c7f0564562f04fa4ed0b38a76eb
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-15 12:31:53 +0530

    BitwiseAndOperatio

---
+++ BitwiseAndOperatio.java
@@ -0,0 +1,8 @@
+public class BitwiseAndOperatio {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a & b;
+        System.out.println(c);
+    }
+}
