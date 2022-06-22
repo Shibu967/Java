3660d3848cd3decb2b4ef9338e76e21bb6d064c0
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-23 00:00:42 +0530

    IfClass

---
+++ IfClass.java
@@ -0,0 +1,10 @@
+public class IfClass {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("you can vote now");
+        }
+
+    }
+
+}
