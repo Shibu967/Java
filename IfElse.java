856d0e596f26498f19b7fd35db5ee4e99973cc3a
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-23 00:17:01 +0530

    IfElse

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
+}
