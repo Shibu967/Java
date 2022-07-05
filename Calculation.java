b4173aa83060eb8fde8f5550acf14dace49c0907
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-07-05 16:00:22 +0530

    Calculation

---
+++ Calculation.java
@@ -0,0 +1,10 @@
+public class Calculation {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int sum = 0;
+        for (int j = 1; j <= 10; j++) {
+            sum = sum + j;
+        }
+        System.out.println("The sum of first 10 natural numbers is " + sum);
+    }
+}
