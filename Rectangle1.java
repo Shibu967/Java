33bed6e01f356783bd0cd56b06cc1f5da6741281
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-07-16 23:24:49 +0530

    Rectangle1

---
+++ Rectangle1.java
@@ -0,0 +1,24 @@
+class Rectangle {
+    int length;
+    int width;
+
+    void insert(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    void calculateArea() {
+        System.out.println(length * width);
+    }
+}
+
+class Rectangle1 {
+    public static void main(String args[]) {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle();
+        r1.insert(11, 5);
+        r2.insert(3, 15);
+        r1.calculateArea();
+        r2.calculateArea();
+    }
+}
