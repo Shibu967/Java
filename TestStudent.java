4335065864c70af36f23e182af2c29a896749c28
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-07-18 12:56:06 +0530

    TestStudent

---
+++ TestStudent.java
@@ -0,0 +1,20 @@
+class Student {
+    int id;
+    String name;
+}
+
+class TestStudent {
+    public static void main(String args[]) {
+        // Creating objects
+        Student s1 = new Student();
+        Student s2 = new Student();
+        // Initializing objects
+        s1.id = 101;
+        s1.name = "Shibu";
+        s2.id = 102;
+        s2.name = "Vipul";
+        // Printing data
+        System.out.println(s1.id + " " + s1.name);
+        System.out.println(s2.id + " " + s2.name);
+    }
+}
