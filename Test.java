d6113e8f6416ddfa0c83bd8127ebc89c67921f3d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-11 21:48:07 +0530

    Test

---
+++ Test.java
@@ -0,0 +1,19 @@
+class Demo {
+    int a = 20;
+    String b = "shibu";
+
+    void Show() {
+        System.out.println(a + " " + b + " ");
+    }
+
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+        Demo r = new Demo();
+        r.Show();
+
+    }
+
+}
