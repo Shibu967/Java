bfa95aa40df3dd92b385390c0fa06b2ef0fa467b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-17 21:56:57 +0530

    Cart

---
+++ Cart.java
@@ -0,0 +1,13 @@
+public class Cart {
+    public static void main(String[] args) {
+        int x = 10, y;
+        if (x < 10)
+            y = 1;
+
+        if (x >= 10)
+            y = 3;
+        System.out.println("y is" + y);
+
+    }
+
+}
