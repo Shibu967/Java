fbbdff469485b664f9e5f04f2d1963dde5254a33
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-17 21:55:49 +0530

    UserInput

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        int b, a;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter value ");
+        b = obj.nextInt();
+        a = obj.nextInt();
+        System.out.println(b);
+        System.out.println(a);
+    }
+
+}
