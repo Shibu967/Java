d0af1cfd8c313914c3c0dfe061b725b6a57ac990
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:08:04 +0530

    Multiplication

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        // initializing variables
+        int num1 = 20, num2 = 10, mult = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Multiplying num1 and num2
+        mult = num1 * num2;
+        System.out.println("Multiplication = " + mult);
+    }
+
+}
