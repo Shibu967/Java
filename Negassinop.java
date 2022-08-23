5e0d33c85e2a755cdca5bb9e453e4a4d2e15f1a5
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:08:30 +0530

    Negassinop

---
+++ Negassinop.java
@@ -0,0 +1,17 @@
+public class Negassinop {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int num1 = 10, num2 = 20;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Subtracting & Assigning values
+        num1 -= num2;
+
+        // Displaying the assigned values
+        System.out.println("num1 = " + num1);
+    }
+
+}
