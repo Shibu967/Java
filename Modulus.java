0d89c37276a1b14b8ba114bfe9b0a227cf71d019
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:06:41 +0530

    Modulus

---
+++ Modulus.java
@@ -0,0 +1,15 @@
+public class Modulus {
+    public static void main(String[] args) {
+        // initializing variables
+        int num1 = 5, num2 = 2, mod = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Remaindering num1 and num2
+        mod = num1 % num2;
+        System.out.println("Remainder = " + mod);
+    }
+
+}
