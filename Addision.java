b8661f43cb0a5c26f499e8ee21b3bba3e3795d6b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:04:26 +0530

    Addision

---
+++ Addision.java
@@ -0,0 +1,14 @@
+public class Addision {
+    public static void main(String[] args) {
+        // initializing variables
+        int num1 = 10, num2 = 20, sum = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // adding num1 and num2
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+}
