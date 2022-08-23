221760b0634b8226d40f27f07b759ec2f7ca9d13
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:05:30 +0530

    Division

---
+++ Division.java
@@ -0,0 +1,14 @@
+public class Division {
+    public static void main(String[] args) {
+        // initializing variables
+        int num1 = 20, num2 = 10, div = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Dividing num1 and num2
+        div = num1 / num2;
+        System.out.println("Division = " + div);
+    }
+}
