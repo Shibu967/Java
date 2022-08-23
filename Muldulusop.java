2bc5697be130c69cd778362885c931ef44419094
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:07:36 +0530

    Muldulusop

---
+++ Muldulusop.java
@@ -0,0 +1,17 @@
+public class Muldulusop {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int num1 = 5, num2 = 3;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Moduling & Assigning values
+        num1 %= num2;
+
+        // Displaying the assigned values
+        System.out.println("num1 = " + num1);
+    }
+
+}
