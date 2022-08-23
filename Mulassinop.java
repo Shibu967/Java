77d73c2162d7bdfac56317acc59385b3fa2f21d7
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:07:10 +0530

    Mulassinop

---
+++ Mulassinop.java
@@ -0,0 +1,16 @@
+public class Mulassinop {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int num1 = 10, num2 = 20;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Multiplying & Assigning values
+        num1 *= num2;
+
+        // Displaying the assigned values
+        System.out.println("num1 = " + num1);
+    }
+}
