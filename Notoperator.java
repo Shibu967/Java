7771c013487497dd760156961f7751312a0a73a5
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-24 00:09:45 +0530

    Notoperator

---
+++ Notoperator.java
@@ -0,0 +1,19 @@
+public class Notoperator {
+    public static void main(String[] args) {
+        // Initializing variables
+        boolean cond = true;
+        int a = 10, b = 1;
+
+        // Displaying values stored in above variables
+        System.out.println("Cond is: " + cond);
+        System.out.println("Var1 = " + a);
+        System.out.println("Var2 = " + b);
+
+        // Displaying values stored in above variables
+        // after applying unary NOT operator
+        System.out.println("Now cond is: " + !cond);
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+    }
+
+}
