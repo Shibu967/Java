cb26ba19d47f0ee25d48cbcb30ac3fbbe5348b09
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-27 22:36:43 +0530

    LogicalNOToperator

---
+++ LogicalNOToperator.java
@@ -0,0 +1,15 @@
+public class LogicalNOToperator {
+    public static void main(String[] args) {
+        // initializing variables
+        int a = 10, b = 1;
+
+        // Displaying a, b, c
+        System.out.println("Var1 = " + a);
+        System.out.println("Var2 = " + b);
+
+        // Using logical NOT operator
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+    }
+
+}
