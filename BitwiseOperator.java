947a233688e1d3801116f57567ef0a554d173d2a
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-27 23:05:23 +0530

    BitwiseOperator

---
+++ BitwiseOperator.java
@@ -0,0 +1,32 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        // Initial values
+        int a = 5;
+        int b = 7;
+
+        // bitwise and
+        // 0101 & 0111=0101 = 5
+        System.out.println("a&b = " + (a & b));
+
+        // bitwise or
+        // 0101 | 0111=0111 = 7
+        System.out.println("a|b = " + (a | b));
+
+        // bitwise xor
+        // 0101 ^ 0111=0010 = 2
+        System.out.println("a^b = " + (a ^ b));
+
+        // bitwise not
+        // ~00000000 00000000 00000000 00000101=11111111 11111111 11111111 11111010
+        // will give 1's complement (32 bit) of 5 = -6
+        System.out.println("~a = " + ~a);
+
+        // can also be combined with
+        // assignment operator to provide shorthand
+        // assignment
+        // a=a&b
+        a &= b;
+        System.out.println("a= " + a);
+    }
+
+}
