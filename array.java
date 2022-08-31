c0f3d21124c4031faccee0debd8a9dcde14857f3
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-31 23:39:31 +0530

    array

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        a[0] = 50;
+        a[1] = 60;
+        a[2] = 70;
+
+        for (int b : a) {
+
+            System.out.println(b);
+        }
+    }
+
+}
