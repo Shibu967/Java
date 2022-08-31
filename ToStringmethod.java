3e5af88992bc638daff5ca567a6c3b9373d2573f
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-08-31 23:42:31 +0530

    ToStringmethod

---
+++ ToStringmethod.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ToStringmethod {
+    public static void main(String[] args) {
+        String a[] = { "Shibu", "Kumari", "yadav", "Gopalganj" };
+        System.out.println(Arrays.toString(a));
+    }
+
+}
