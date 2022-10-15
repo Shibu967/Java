1e0fa472956e07de9693a748d0f17b3999759e54
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-16 01:58:14 +0530

    Dimond

---
+++ Dimond.java
@@ -0,0 +1,16 @@
+public class Dimond {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
