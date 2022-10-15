be306ca1b5affa381dbd01ccb7017e288c227841
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-16 02:01:46 +0530

    Trangle

---
+++ Trangle.java
@@ -0,0 +1,14 @@
+public class Trangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
