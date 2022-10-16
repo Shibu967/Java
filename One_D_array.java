951f80a635dab8ba936cf4539be675bd6e7a06e4
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-16 23:39:18 +0530

    One_D_array

---
+++ One_D_array.java
@@ -0,0 +1,10 @@
+public class One_D_array {
+    public static void main(String[] args) {
+
+        int[] a = { 20, 30, 40, 50, 60 };
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+}
