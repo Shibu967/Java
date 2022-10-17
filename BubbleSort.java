727b624d32b8806a814e038a7635e87ae5ae5aa9
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-17 23:24:45 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a = { 36, 19, 29, 12, 5 };
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+}
