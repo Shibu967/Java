d420b00a9cbe2db635d60c9b728667a856939619
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-19 02:16:16 +0530

    String_reverse

---
+++ String_rev.java
@@ -0,0 +1,12 @@
+public class String_rev {
+    public static void main(String[] args) {
+        String s = "Madam";
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--)
+
+        {
+            rev = rev + s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
