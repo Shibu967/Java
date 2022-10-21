d728277e9b539331ed0f2c056ea49df49b9c1eeb
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 23:57:14 +0530

    Swap tow number

---
+++ Swaptownumber.java
@@ -0,0 +1,11 @@
+public class Swaptownumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+
+}
