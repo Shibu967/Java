2f22e0a5d8ce82bc585becd444ce10e81647f1b2
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 14:38:56 +0530

    DefultConstructor

--- DefultConstructor.java
+++ DefultConstructor.java
@@ -1,28 +1,11 @@
-class A {
-    int a;
-    String name;
-
-    /*
-     * A()// this is constructor
-     * {
-     * a = 0;
-     * name = null;
-     * }
-     */
-
-    void show()// this is normal method
-    {
-        System.out.println(a + " " + name);
-    }
-
-}
-
 public class DefultConstructor {
+    int i;
+
     public static void main(String[] args) {
-        A ref = new A();// object creataion
-        ref.show();
+
+        DefultConstructor d = new DefultConstructor();
+        System.out.println(d.i);
 
     }
 
 }
-// this is constructor coding
