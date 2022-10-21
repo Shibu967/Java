c1982593e61e5b09fa0a1a98f0836b1822b5ddde
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 19:48:45 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+
+    }
+
+}
