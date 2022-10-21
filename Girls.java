462fc20074fa7c00b969aa7dbed8f5e1f31f92c8
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 14:39:23 +0530

    Girls

---
+++ Girls.java
@@ -0,0 +1,22 @@
+public class Girls {
+
+    public void shibu() {
+        System.out.println("She is studing");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        // Girls.rb = new Girls();
+        // rb.shibu();
+        Birds b = new Birds();
+        b.fly();
+    }
+
+}
+
+class Birds {
+    void fly() {
+        System.out.println("i am flying");
+
+    }
+}
