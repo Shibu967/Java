ece5f1628cf7f920cbc3232feec15d2743a55de1
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 23:17:19 +0530

    Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,21 @@
-import java.util.Scanner;
-
 public class Palindrome {
     public static void main(String[] args) {
-        int n, s = 0, c, r;
-        System.out.println("Enter any number");
-        Scanner ref = new Scanner(System.in);
-        n = ref.nextInt();
-        c = n;
+
+        int n = 121;
+        int s = 0;
+        int r;
+        int temp;
+        temp = n;
         while (n > 0) {
             r = n % 10;
             s = (s * 10) + r;
             n = n / 10;
-
         }
-        if (c == s)
-            System.out.println("palindrome");
-        else
-            System.out.println("not palindrome");
-    }
+        if (temp == s) {
+            System.out.println("palindrome number");
+        } else {
+            System.out.println("not palindrome number");
+        }
 
+    }
 }
