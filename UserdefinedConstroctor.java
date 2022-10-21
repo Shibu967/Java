bde4481ecca0cc936ab3f1be3bfd5aaa17f02afc
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 14:40:03 +0530

    UserdefinedConstroctor

---
+++ UserdefinedConstroctor.java
@@ -0,0 +1,12 @@
+public class UserdefinedConstroctor {
+
+    UserdefinedConstroctor() {
+        System.out.println("no arg constructor");
+
+    }
+
+    public static void main(String[] args) {
+        UserdefinedConstroctor u = new UserdefinedConstroctor();
+    }
+
+}
