d15046c4cad343f3e2e884f41962ad3786482c3d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-21 14:37:45 +0530

    Animal

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    String color;
+    int age;
+
+    public static void main(String[] args) {
+        Animal Buzo = new Animal();
+        Buzo.color = "black";
+        Buzo.age = 10;
+        System.out.println(Buzo.color + " " + Buzo.age);
+
+    }
+
+}
+// Object inialiation by reference variable
