9b06dd225d7511e4f318f1c55bb5cd03f23a7949
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 22:15:10 +0530

    Print Even Number

---
+++ PrintEvenNumber.java
@@ -0,0 +1,10 @@
+public class PrintEvenNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= 10; i = i + 2) {
+
+            System.out.print(i + " ");
+        }
+    }
+
+}
