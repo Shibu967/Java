008f94160a2c8b6aaca09bb3813321708498373b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 19:16:59 +0530

    Create Object Example1

---
+++ CreateObjectExample1.java
@@ -0,0 +1,13 @@
+public class CreateObjectExample1 {
+
+    void show() {
+        System.out.println("Welcome to javaTpoint");
+    }
+
+    public static void main(String[] args) {
+        // creating an object using new keyword
+        CreateObjectExample1 obj = new CreateObjectExample1();
+        // invoking method using the object
+        obj.show();
+    }
+}
