9b41bd01370846e0f5aef9f1bb8de49bcd2c8bea
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 22:15:39 +0530

    Print Odd Number

---
+++ PrintOddNumber.java
@@ -0,0 +1,10 @@
+public class PrintOddNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i <= 10; i = i + 2) {
+
+            System.out.print(i);
+        }
+    }
+
+}
