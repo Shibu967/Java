9ef223ed99655bd2f83f36ee42513a2e9b8fdb12
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 19:15:31 +0530

    Ascii Value A to Z

---
+++ AsciiValueAtoZ.java
@@ -0,0 +1,7 @@
+public class AsciiValueAtoZ {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println(" The ASCII value of " + (char) i + "  =  " + i);
+        }
+    }
+}
