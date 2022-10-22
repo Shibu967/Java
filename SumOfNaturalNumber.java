0b7e07b6327b968f9355feea44a2a4c2cd08916d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 22:17:50 +0530

    Sum Of Natural Number

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,12 @@
+public class SumOfNaturalNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i;
+
+        }
+        System.out.println(sum);
+    }
+
+}
