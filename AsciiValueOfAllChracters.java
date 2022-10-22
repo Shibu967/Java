e87407be99309c43f79f0831dc854bf4da4a49cf
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 19:16:25 +0530

    Ascii Value Of All Chracters

---
+++ AsciiValueOfAllChracters.java
@@ -0,0 +1,7 @@
+public class AsciiValueOfAllChracters {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 255; i++) {
+            System.out.println(" The ASCII value of " + (char) i + "  =  " + i);
+        }
+    }
+}
