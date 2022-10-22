83d6252ab3bcaddfec5f51e9bb548b93a1f21d54
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-22 22:18:13 +0530

    Table

---
+++ Table.java
@@ -0,0 +1,10 @@
+public class Table {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print(n * i + " ");
+        }
+    }
+
+}
