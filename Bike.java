a41c6aca4b5fa01123513061a2465394cb6761e1
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-23 21:07:13 +0530

    Bike

---
+++ Bike.java
@@ -0,0 +1,19 @@
+class Vehicle {
+    // defining a method
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+// Creating a child class
+class Bike extends Vehicle {
+    // defining the same method as in the parent class
+    void run() {
+        System.out.println("Bike is running safely");
+    }
+
+    public static void main(String args[]) {
+        Bike obj = new Bike();// creating object
+        obj.run();// calling method
+    }
+}
