116005a44f5995e32b27ccc5886e6447c5218f34
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:21:20 +0530

    Byte Constroctor

---
+++ ByteConstroctor.java
@@ -0,0 +1,8 @@
+public class ByteConstroctor {
+    public static void main(String[] args) {
+        byte[] b = { 101, 102, 103 };
+        String s2 = new String(b);
+        System.out.println(s2);
+    }
+
+}
