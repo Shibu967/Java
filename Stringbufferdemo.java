9063ce98f85ff553af6fb169d427fa90b5c1a627
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:26:53 +0530

    String buffer demo

---
+++ Stringbufferdemo.java
@@ -0,0 +1,8 @@
+public class Stringbufferdemo {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Shibu");
+        System.out.println(sb.capacity());
+
+    }
+
+}
