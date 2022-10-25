3f7bc51db30db2e6ef482e4f11539a7ada89153f
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:22:09 +0530

    CompareTo

---
+++ CompareTo.java
@@ -0,0 +1,9 @@
+public class CompareTo {
+
+    public static void main(String[] args) {
+        String s1 = "a";// 97
+        String s2 = "A";// 65
+        System.out.println(s1.compareTo(s2));
+
+    }
+}
