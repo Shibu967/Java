44994853a1a432af9cb8bf7ae506b4472f40b864
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:22:50 +0530

    Compare To gnorCase

---
+++ CompareToIgnorCase.java
@@ -0,0 +1,9 @@
+public class CompareToIgnorCase {
+    public static void main(String[] args) {
+        String s1 = "S";
+        String s2 = "Sb";
+        System.out.println(s1.compareToIgnoreCase(s2));
+
+    }
+
+}
