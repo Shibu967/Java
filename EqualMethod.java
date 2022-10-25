b840510b5b96068e18903ea9ea4ab7a97682414f
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:24:16 +0530

    Equal Method

---
+++ EqualMethod.java
@@ -0,0 +1,9 @@
+public class EqualMethod {
+    public static void main(String[] args) {
+        String s1 = "Shibu";
+        String s2 = "Shibu";
+        System.out.println(s1.equals(s2));
+
+    }
+
+}
