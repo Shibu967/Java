bfae7a96c7f2e756d4df88653d07e29a71b4131c
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:25:00 +0530

    EqualsIgnoreCase

---
+++ EqualsIgnoreCase.java
@@ -0,0 +1,9 @@
+public class EqualsIgnoreCase {
+    public static void main(String[] args) {
+        String s1 = "Shibu";
+        String s2 = "SHIBU";
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+
+}
