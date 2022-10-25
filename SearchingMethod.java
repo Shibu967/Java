22676bac1f952759d84b635ba82649e6392c005d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:25:53 +0530

    Searching Method.

---
+++ SearchingMethod.java
@@ -0,0 +1,15 @@
+public class SearchingMethod {
+    public static void main(String[] args) {
+        String s = "Shibu";
+        System.out.println(s.indexOf("s"));
+        System.out.println(s.indexOf("b"));
+        System.out.println(s.lastIndexOf("u"));
+        System.out.println(s.charAt(3));
+        System.out.println(s.indexOf("e"));
+        System.out.println(s.contains("s"));
+        System.out.println(s.startsWith("e"));
+        System.out.println(s.endsWith("u"));
+
+    }
+
+}
