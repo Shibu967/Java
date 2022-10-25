4444eab9600ab02fb35da2a9f6c906721aac7cbb
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:21:46 +0530

    Char Constrocter

---
+++ CharConstrocter.java
@@ -0,0 +1,8 @@
+public class CharConstrocter {
+    public static void main(String[] args) {
+        char[] b = { 'a', 'b', 'c' };
+        String s2 = new String(b);
+        System.out.println(s2);
+    }
+
+}
