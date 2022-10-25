53188821a5adfb132eb1673bcba0c5148750e26f
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:26:17 +0530

    String Add

---
+++ StringAdd.java
@@ -0,0 +1,16 @@
+public class StringAdd {
+    public static void main(String[] args) {
+        String s1 = "Shibu";
+        String s2 = " SHIBU";
+        System.out.println(s1 + s2);
+        System.out.println(s1 + 10);
+        System.out.println(s1 + 10 + 20);
+        System.out.println(10 + s1 + 20);
+        System.out.println(s1 + s1 + 20);
+        System.out.println(20 + 10 + s1);
+        System.out.println(s1 + 20 / 20);
+        System.out.println(s1 + 10 * 20);
+
+    }
+
+}
